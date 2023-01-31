ece8b8944d5d4b8dbef1a8630d7a6fafc6452f06
Aditya Bhattacharya <adib0073@gmail.com>
2023-01-31 13:58:06 +0100

    added background video rendering for landing page

--- app-ui/client/main.jsx
+++ app-ui/client/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import { App } from '/imports/ui/App';
+//import { App } from '/imports/ui/App';
+import { LandingPage } from '../imports/ui/LandingPage';
 import './main.css';
 
 Meteor.startup(() => {
-  render(<App/>, document.getElementById('react-target'));
+  //render(<App/>, document.getElementById('react-target'));
+  render(<LandingPage/>, document.getElementById('react-target'));
 });
---
+++ app-ui/imports/ui/LandingPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export const LandingPage = () => {
+
+    return (
+    <div className="app-container">
+        <div className='video-container'>
+            <video autoPlay loop muted width="100%" height="100%">
+                <source src="https://drive.google.com/uc?export=download&id=11iei1XUn8nlaKqelYwyueplnj42-JjQb" type="video/mp4"/>
+            </video>
+
+
+        </div>
+    
+    
+    
+    </div >    
+    );
+
+};
