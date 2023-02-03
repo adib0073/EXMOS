878f8416742407cfc85cca898881dcb3cf451c0b
Aditya Bhattacharya <adib0073@gmail.com>
2023-02-03 12:06:20 +0100

    changed landing page styling

--- app-ui/imports/ui/LandingPage.jsx
+++ app-ui/imports/ui/LandingPage.jsx
@@ -1,36 +1,41 @@
 import React from 'react';
-import { useNavigate  } from 'react-router-dom';
-import { Input, Select } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 
 
 export const LandingPage = () => {
-    const navigate = useNavigate ();
+    const navigate = useNavigate();
     const selectedDashType = () => {
         navigate('/dashboard');
     }
     return (<div className="app-container">
         <div className="lp-container">
             <div className="lp-container-inner">
-                <img className="lp-container-image" src="https://events.transperfect.com/wp-content/uploads/2021/09/DataForce-Logo-White.png" />
+                <img className="lp-container-image" src="https://raw.githubusercontent.com/adib0073/HCI_design/main/EXMOS2023/exmos_logo_1.png" />
                 <h1 className="lp-container-headerfont-1">
                     Explanatory Model Steering for Healthcare
                 </h1>
                 <br />
                 <h1 className="lp-container-headerfont-2">
-                    An Interactive Machine Learning platform for healthcare experts for personalized predictions
+                    An Interactive Explainable Machine Learning Platform for Healthcare Experts for Personalized Predictions
                 </h1>
-                <Input placeholder="Please enter your user id" />
-                <br />
-                <Input placeholder="Please enter your assigned code" />
-                <br />
-                <br />
-                <button onClick={selectedDashType}>Let's Start</button>
+                <div className="lp-container-entry">
+                    <input className="lp-container-entry-input" placeholder="Please enter your user id" />
+                    <br />
+                    <select className="lp-container-entry-input"  >
+                        <option value="" disabled selected>Select your assigned cohort</option>
+                        <option value="DCE">Cohort-1</option>
+                        <option value="MCE">Cohort-2</option>
+                        <option value="HYB">Cohort-3</option>
+                    </select>
+                    <br />
+                    <button className="lp-container-entry-button" onClick={selectedDashType}>Let's Start</button>
+                </div>
             </div>
         </div>
         <div className='video-container'>
-            <video autoPlay loop muted width="100%" height="100%">
-                <source src="https://drive.google.com/uc?export=download&id=11iei1XUn8nlaKqelYwyueplnj42-JjQb" type="video/mp4" />
+            <video autoPlay loop muted >
+                <source src="https://github.com/adib0073/HCI_design/blob/main/EXMOS2023/website_bg.mp4?raw=true" type="video/mp4" />
             </video>
         </div>
     </div >
