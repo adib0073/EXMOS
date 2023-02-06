9b34bb51cc32a6db982401d6d9d2e5e0aa6bb074
Aditya Bhattacharya <adib0073@gmail.com>
2023-02-06 19:31:23 +0100

    added design for chart component 1

--- app-ui/imports/ui/dashboards/dce.jsx
+++ app-ui/imports/ui/dashboards/dce.jsx
@@ -11,7 +11,17 @@ export const DCE = () => {
                 <div className="dce-container-left-col">
                     <div className="dce-container-left-r1">
                         <div className="dce-container-left-r1c1">
-                            Left Col - Row 1 - Sub Col 1
+                            <div className="chart-title-box">
+                                <div className="chart-title">
+                                        Prediction Accuracy
+                                </div>
+                                <div className="chart-icons">
+                                    Chart Icon/Logos
+                                </div>
+                            </div>
+                            <div className="chart-container">
+                                Chart Canvas for Doughnut Chart
+                            </div>
                         </div >
                         <div className="dce-container-left-r1c2">
                             Left Col - Row 1 - Sub Col 2
