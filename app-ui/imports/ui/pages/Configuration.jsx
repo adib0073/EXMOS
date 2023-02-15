3da649912dbd914c11eb4be4eba161fcb5bb3d4d
Aditya Bhattacharya <adib0073@gmail.com>
2023-02-15 18:03:02 +0100

    completed config tab functionality

--- app-ui/imports/ui/pages/Configuration.jsx
+++ app-ui/imports/ui/pages/Configuration.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { NavBar } from '../components/NavBar/NavBar.jsx';
 import './Configuration.css'
 
 export const Configuration = ({ user, setUser }) => {
-    
-    const handleTabView = () => {
-        console.log("Pressed");
+    const [activeTab, setActiveTab] = useState("tab1");
+    const handleTab1View = () => {
+        setActiveTab("tab1");
+    };
+    const handleTab2View = () => {
+        setActiveTab("tab2");
     };
     return (
         <>
             <NavBar user={user} />
             <div className="config-container">
-                <div className="tab">
-                    <button className="tablinks" onClick={handleTabView}>Configure Features</button>
-                    <button className="tablinks" onClick={handleTabView}>Correct Data Issues</button>
-                </div>
+                <ul className="tab">
+                    <li className={activeTab === "tab1" ? "active" : ""}
+                        onClick={handleTab1View}
+                    >Configure Features</li>
+                    <li className={activeTab === "tab2" ? "active" : ""}
+                        onClick={handleTab2View}
+                    >Correct Data Issues</li>
+                </ul>
                 <div className="config-display">
+                    {activeTab === "tab1" ? "Configure Features as component" : "Correct data issues as component"}
                 </div>
             </div>
         </>);
