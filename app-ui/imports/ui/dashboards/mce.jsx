53a7dfb4f75e7052d70070e6d6120ada50c5cfab
Aditya Bhattacharya <adib0073@gmail.com>
2023-03-10 21:47:02 +0100

    basic mce dashboard flex layout

--- app-ui/imports/ui/dashboards/mce.jsx
+++ app-ui/imports/ui/dashboards/mce.jsx
@@ -12,16 +12,81 @@ import { RectBlock } from '../components/Icons/RectBlock.jsx';
 import { BASE_API, DATA_SUMMARY_DEFAULT_MODEL, DATA_ISSUE_FRIENDLY_NAMEs } from '../Constants.jsx';
 import axios from 'axios';
 
+const GetPredChartValue = ({ userid, setAccChartVals }) => {
+
+    axios.get(BASE_API + '/getpredchartvalues/?user=' + userid)
+        .then(function (response) {
+            //console.log(response.data["OutputJson"]);
+            setAccChartVals({
+                accuracy: response.data["OutputJson"]["Accuracy"],
+                nsamples: response.data["OutputJson"]["NumSamples"],
+                nfeats: response.data["OutputJson"]["NumFeatures"],
+                pct: response.data["OutputJson"]["ScoreChange"]
+            });
+
+        }).catch(function (error) {
+            console.log(error);
+        });
+}
+
 export const MCE = ({ user }) => {
     var userid = user.id;
     var cohort = user.cohort;
     if (userid == null || userid == "") {
         userid = window.localStorage.getItem('userid');
     }
+
+    const accuracyChartRef = useRef();
+    const [accChartVals, setAccChartVals] = useState({ accuracy: 0, nsamples: 0, nfeats: 0, pct: 0 });
+
+    useEffect(() => {
+        GetPredChartValue({ userid, setAccChartVals });
+    }, []);
+
+    const greenFont = "#449231";
+    const redFont = "#D64242";
+
     return (
         <>
             <NavBar user={user} />
-            Hello MCE
+            <div className="mce-container">
+                <div className="mce-container-r1">
+                    <div className="mce-container-r1c1">
+                        <div className="chart-title-box">
+                            <div className="chart-title">
+                                Prediction Accuracy
+                            </div>
+                            <div className="chart-icons">
+                                <InfoLogo setButtonPopup={false} setChartIndex={0} index={3} />
+                            </div>
+                        </div>
+                        <div className="chart-container">
+                            <div className='chart-container-viz'>
+                                <DoughnutChart accuracy={accChartVals.accuracy} chartRef={accuracyChartRef} />
+                            </div>
+                            <div className='chart-container-info'>
+                                <HollowBullet /> &nbsp;Training Samples : <b>{accChartVals.nsamples}</b>
+                            </div>
+                            <div className='chart-container-info'>
+                                <HollowBullet /> &nbsp;Features Considered : <b>{accChartVals.nfeats}</b>
+                            </div>
+                            <div className='chart-container-info'>
+                                <span style={{ color: (accChartVals.pct > 0) ? greenFont : redFont }}>
+                                    {(accChartVals.pct > 0) ? <UpGreenArrow /> : <DownRedArrow />}
+                                    <b> &nbsp;{accChartVals.pct}% </b>
+                                </span>
+                                from previous score
+                            </div>
+                        </div>
+                    </div >
+                    <div className="mce-container-r1c2">
+                        Chart 2
+                    </div>
+                </div>
+                <div className="mce-container-r2">
+                    Bottom Charts
+                </div>
+            </div>
         </>
     );
 
