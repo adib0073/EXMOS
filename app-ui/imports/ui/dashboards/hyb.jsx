740d58bbad1c0af957d67d2374e51e8929dc5979
Aditya Bhattacharya <adib0073@gmail.com>
2023-04-07 16:26:05 +0200

    upper half charts included for hybrid dashboard

--- app-ui/imports/ui/dashboards/hyb.jsx
+++ app-ui/imports/ui/dashboards/hyb.jsx
@@ -13,7 +13,7 @@ import { BASE_API, DATA_SUMMARY_DEFAULT_MODEL, DATA_ISSUE_FRIENDLY_NAMEs } from 
 import axios from 'axios';
 import GaugeChart from 'react-gauge-chart'
 import { ContinuousDistribution } from '../components/PatientSummaryPlot/ContinuousDistribution.jsx';
-import { Tooltip } from 'antd';
+import { Tooltip, Spin } from 'antd';
 import { tooltipEnglishContent } from '../tooltipContent/tooltipEnglishContent.jsx';
 import { tooltipSloveneContent } from '../tooltipContent/tooltipSloveneContent.jsx';
 
@@ -32,7 +32,35 @@ const GetPredChartValue = ({ userid, setAccChartVals }) => {
         }).catch(function (error) {
             console.log(error);
         });
-}
+};
+
+const GetTopDecisionRules = ({ userid, setTopRules, setRuleView }) => {
+    //console.log(userid);
+    axios.get(BASE_API + '/getdecisionrules/?user=' + userid)
+        .then(function (response) {
+            //console.log(response.data["OutputJson"]);
+            setTopRules(response.data["OutputJson"]);
+            setRuleView(response.data["OutputJson"]["diabetic"])
+        }).catch(function (error) {
+            console.log(error);
+        });
+};
+
+const GetKIChartValue = ({ userid, setKiChartVals }) => {
+
+    axios.get(BASE_API + '/getkeyinsights/?user=' + userid)
+        .then(function (response) {
+            //console.log(response.data["OutputJson"]);
+            setKiChartVals({
+                "pct_list": response.data["OutputJson"]["pct_list"],
+                "input_list": response.data["OutputJson"]["input_list"],
+                "insight_list": response.data["OutputJson"]["insight_list"]
+            });
+
+        }).catch(function (error) {
+            console.log(error);
+        });
+};
 
 const PostInteractions = ({ userid, cohort, interactioData }) => {
     axios.post(BASE_API + '/trackinteractions', {
@@ -73,6 +101,17 @@ export const HYB = ({ user }) => {
     const accuracyChartRef = useRef();
     // Set UseStates
     const [accChartVals, setAccChartVals] = useState({ accuracy: 0, nsamples: 0, nfeats: 0, pct: 0 });
+    const [activeFilter, setActiveFilter] = useState("diabetic");
+    const [topRules, setTopRules] = useState({
+        "diabetic": null,
+        "non-diabetic": null
+    });
+    const [ruleView, setRuleView] = useState(["Not found"]);
+    const [kiChartVals, setKiChartVals] = useState({
+        "pct_list": [0, 0, 0, 0],
+        "input_list": ["Not available. ", "Not available. ", "Not available. ", "Not available. "],
+        "insight_list": ["Try later", "Try later", "Try later", "Try later"],
+    });
 
     const greenFont = "#449231";
     const redFont = "#D64242";
@@ -80,8 +119,15 @@ export const HYB = ({ user }) => {
     useEffect(() => {
         GetPredChartValue({ userid, setAccChartVals });
         //GetFeatureImportance({ userid, setFeatureImportance });
-        //GetTopDecisionRules({ userid, setTopRules, setRuleView });
+        GetTopDecisionRules({ userid, setTopRules, setRuleView });
+        GetKIChartValue({ userid, setKiChartVals });
     }, []);
+
+    // Top Rules
+    const handleFilterClick = (category) => {
+        setActiveFilter(category);
+        setRuleView(topRules[category]);
+    };
 
     // Hover time for interaction data
     var startTime, endTime;
@@ -118,6 +164,13 @@ export const HYB = ({ user }) => {
 
         PostInteractions({ userid, cohort, interactioData });
     };
+
+    // Loading Indicator
+    const loadingIndicator = (
+        <>
+            <Spin tip="Loading ...  " size="small" />
+        </>
+    );
 
     // Language variable
     // TO-DO: Take language preferred as input
@@ -181,10 +234,96 @@ export const HYB = ({ user }) => {
                         </div>
                     </div>
                     <div className="hyb-container-r1c2">
-                        row 1 column 2
+                        <div className="chart-title-box">
+                            <div className="chart-title">
+                                Top Decision Rules
+                            </div>
+                            <Tooltip
+                                placement="bottom"
+                                title={lang.mce.decisionRule.title}
+                                overlayStyle={{ maxWidth: '500px' }}
+                            >
+                                <div className="chart-icons">
+                                    <InfoLogo />
+                                </div>
+                            </Tooltip>
+                        </div>
+                        <div className="chart-container-mce">
+                            {topRules.diabetic == null || topRules['non-diabetic'] == null ?
+                                loadingIndicator :
+                                <>
+                                    <div className="top-rules-filter">
+                                        <div className={activeFilter === "diabetic" ? "top-rules-filter-left-active" : "top-rules-filter-left"} onClick={() => { handleFilterClick("diabetic") }}>
+                                            Diabetic
+                                        </div>
+                                        <div className={activeFilter === "non-diabetic" ? "top-rules-filter-right-active" : "top-rules-filter-right"} onClick={() => { handleFilterClick("non-diabetic") }}>
+                                            Non-diabetic
+                                        </div>
+                                    </div>
+                                    <div className="top-rules-viz" onClick={() => { handleVizClick("DecisionRules", "Viz") }} onMouseEnter={() => { handleMouseIn() }} onMouseLeave={() => { handleMouseOut("DecisionRules", "Viz") }}>
+                                        {ruleView.map((item, index) => {
+                                            return (
+                                                <div className="top-rules-viz-item" key={index}>
+                                                    <b>{item}</b>
+                                                </div>
+                                            );
+                                        })}
+                                    </div>
+                                </>
+                            }
+                        </div>
                     </div>
                     <div className="hyb-container-r1c3">
-                        row 1 column 3
+                        <div className="chart-title-box">
+                            <div className="chart-title">
+                                Key Insights
+                            </div>
+                            <Tooltip
+                                placement="bottom"
+                                title={lang.dce.keyInsights.title}
+                                overlayStyle={{ maxWidth: '500px' }}
+                            >
+                                <div className="chart-icons">
+                                    <InfoLogo />
+                                </div>
+                            </Tooltip>
+                        </div>
+                        <div className="chart-container">
+                            <div className="capsule-container" onClick={() => { handleVizClick("KeyInsights", "Viz") }} onMouseEnter={() => { handleMouseIn() }} onMouseLeave={() => { handleMouseOut("KeyInsights", "Viz") }}>
+                                <div className="capsule-div">
+                                    <div className="capsule-div-left">
+                                        {kiChartVals["pct_list"][0]}%
+                                    </div>
+                                    <div className="capsule-div-right">
+                                        {kiChartVals["input_list"][0]}  <b>{kiChartVals["insight_list"][0]}</b>
+                                    </div>
+                                </div>
+                                <div className="capsule-div">
+                                    <div className="capsule-div-left">
+                                        {kiChartVals["pct_list"][1]}%
+                                    </div>
+                                    <div className="capsule-div-right">
+                                        {kiChartVals["input_list"][1]}  <b>{kiChartVals["insight_list"][1]}</b>
+                                    </div>
+                                </div>
+                                <div className="capsule-div">
+                                    <div className="capsule-div-left">
+                                        {kiChartVals["pct_list"][2]}%
+                                    </div>
+                                    <div className="capsule-div-right">
+                                        {kiChartVals["input_list"][2]}  <b>{kiChartVals["insight_list"][2]}</b>
+                                    </div>
+                                </div>
+                                <div className="capsule-div">
+                                    <div className="capsule-div-left">
+                                        {kiChartVals["pct_list"][3]}%
+                                    </div>
+                                    <div className="capsule-div-right">
+                                        {kiChartVals["input_list"][3]}  <b>{kiChartVals["insight_list"][3]}</b>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="hyb-container-r2">
